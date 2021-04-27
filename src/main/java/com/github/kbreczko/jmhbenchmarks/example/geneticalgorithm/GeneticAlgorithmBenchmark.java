c26072a6e0434cf2abdb29016f5539a87d2f2ea5
Kamil Breczko <kamil.breczko@gmail.com>
2021-04-27 20:39:41 +0200

    create benchmark

---
+++ src/main/java/com/github/kbreczko/jmhbenchmarks/example/geneticalgorithm/GeneticAlgorithmBenchmark.java
@@ -0,0 +1,33 @@
+package com.github.kbreczko.jmhbenchmarks.example.geneticalgorithm;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(value = {Mode.All})
+@Fork(value = 1)
+@Warmup(iterations = 5)
+@Measurement(iterations = 15)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+public class GeneticAlgorithmBenchmark {
+
+    @State(Scope.Benchmark)
+    public static class Plan {
+        int populationSize;
+        String solution;
+        int maxGenerations;
+
+        @Setup(Level.Iteration)
+        public void setUp() {
+            this.solution = "1011000100000100010000100000100111001000000100000100000000001111";
+            this.populationSize = 50;
+            this.maxGenerations = 50;
+        }
+    }
+
+//    @Benchmark
+    public Result runAlgorithm(Plan plan) {
+        final SimpleGeneticAlgorithm simpleGeneticAlgorithm = new SimpleGeneticAlgorithm();
+        return simpleGeneticAlgorithm.runAlgorithm(plan.populationSize, plan.solution, plan.maxGenerations);
+    }
+}
