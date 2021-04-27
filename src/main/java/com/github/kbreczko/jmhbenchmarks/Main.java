45136dc8598538001c14c7e3a52f3257cdfa2628
Kamil Breczko <kamil.breczko@deviniti.com>
2021-04-27 20:31:03 +0200

    init project

---
+++ src/main/java/com/github/kbreczko/jmhbenchmarks/Main.java
@@ -0,0 +1,10 @@
+package com.github.kbreczko.jmhbenchmarks;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        org.openjdk.jmh.Main.main(args);
+    }
+}
