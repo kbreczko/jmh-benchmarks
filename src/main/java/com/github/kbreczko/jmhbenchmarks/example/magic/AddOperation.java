8a505a388801e4a9d98ff5033df9e725d897ce1c
Kamil Breczko <kamil.breczko@gmail.com>
2021-04-27 20:44:01 +0200

    add magic benchmark

---
+++ src/main/java/com/github/kbreczko/jmhbenchmarks/example/magic/AddOperation.java
@@ -0,0 +1,18 @@
+package com.github.kbreczko.jmhbenchmarks.example.magic;
+
+import java.math.BigDecimal;
+
+public class AddOperation {
+
+    public BigDecimal add(BigDecimal number1, BigDecimal number2) {
+        if(number1 == null){
+            throw new IllegalArgumentException();
+        }
+
+        if(number2 == null){
+            throw new IllegalArgumentException();
+        }
+
+        return number1.add(number2);
+    }
+}
---
+++ src/main/java/com/github/kbreczko/jmhbenchmarks/example/magic/MagicBenchmark.java
@@ -0,0 +1,38 @@
+package com.github.kbreczko.jmhbenchmarks.example.magic;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+
+@BenchmarkMode(value = {Mode.All})
+@Fork(value = 1)
+@Warmup(iterations = 5)
+@Measurement(iterations = 15)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class MagicBenchmark {
+
+    @State(Scope.Benchmark)
+    public static class SumPlan {
+        public List<BigDecimal> numbers;
+        public BigDecimal sum;
+
+        @Setup(Level.Iteration)
+        public void setUp() {
+            this.numbers = DoubleStream.iterate(0.5, x -> x + 10)
+                    .mapToObj(BigDecimal::new)
+                    .limit(10_000)
+                    .collect(Collectors.toList());
+            this.sum = BigDecimal.valueOf(499_955_000);
+        }
+    }
+
+    @Benchmark
+    public BigDecimal doMagic(SumPlan state) {
+        final Calculator calculator = new Calculator(state.sum);
+        return calculator.sum(state.numbers);
+    }
+}
