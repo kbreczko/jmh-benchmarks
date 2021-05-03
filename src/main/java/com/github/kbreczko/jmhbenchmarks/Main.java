bd7df38e814bc541476512a2e577061e944cfd7e
Kamil Breczko <kamil.breczko@gmail.com>
2021-05-03 10:16:09 +0200

    add javadoc

--- src/main/java/com/github/kbreczko/jmhbenchmarks/Main.java
+++ src/main/java/com/github/kbreczko/jmhbenchmarks/Main.java
@@ -2,6 +2,11 @@ package com.github.kbreczko.jmhbenchmarks;
 
 import java.io.IOException;
 
+
+/**
+ * Uruchomienie testów wydajnościowych w celu porównania Java 8 z Java 11.
+ * Założenia: Maksymalny rozmiar sterty to 4GB.
+ */
 public class Main {
 
     public static void main(String[] args) throws IOException {
